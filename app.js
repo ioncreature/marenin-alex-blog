7a27ba89524315cc679577519463ec05ef7defed
Marenin Alex <great_muchacho@mail.ru>
2012-08-27 17:24:52 +0400

    added FileStorage

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require( 'express' ),
 	routes = require( './routes' ),
 	http = require( 'http' ),
-	path = require( 'path' );
+	path = require( 'path' ),
+	FileStorage = require( './lib/FileStorage.js' );
 
 var app = express();
 
@@ -9,13 +10,14 @@ app.configure( function(){
 	app.set( 'port', 3000 );
 	app.set( 'views', __dirname + '/views' );
 	app.set( 'view engine', 'jade' );
+	app.set( 'storage path', __dirname + '/storage/data.json' );
 	app.use( express.favicon() );
 	app.use( express.logger( 'dev' ) );
 	app.use( express.bodyParser() );
 	app.use( express.methodOverride() );
 	app.use( app.router );
 	app.use( express.static(path.join(__dirname, 'public')) );
-} );
+});
 
 app.configure( 'development', function(){
 	app.use( express.errorHandler() );
