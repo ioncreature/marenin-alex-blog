be95e9a7d1f6f869711f5d75a5c4dd5ed6e1a1d7
alexander.marenin <great_muchacho@mail.ru>
2012-11-01 15:54:13 +0400

    updated app.js

--- app.js
+++ app.js
@@ -16,41 +16,45 @@ app.configure( function(){
 	app.set( 'port', 3000 );
 	app.set( 'views', __dirname + '/views' );
 	app.set( 'view engine', 'jade' );
-	app.set( 'storage path', __dirname + '/storage/data.json' );
+	app.set( 'storage path', path.join(__dirname, 'storage', 'data.json') );
+	app.set( 'storage', new FileStorage(app.get('storage path')) );
+    app.set( 'storage' ).load();
+
 	app.use( express.favicon() );
 	app.use( express.logger( 'dev' ) );
 	app.use( express.bodyParser() );
 	app.use( express.methodOverride() );
 	app.use( app.router );
-	app.use( express.static(path.join(__dirname, 'public')) );
+	app.use( '/public/', express.static(path.join(__dirname, 'public')) );
 });
 
 app.configure( 'development', function(){
 	app.use( express.errorHandler() );
 });
 
-var storage = new FileStorage( app.get('storage path') );
-storage.load();
-
 
 // ROUTES
-var articles = storage.get( 'articles' ) || {};
+var articles = app.get( 'storage' ).get( 'articles' ) || {};
 
 app.get( '/article/:id', function( req, res ){
 	var article = articles[req.params.id];
 
-	res.render( 'article', {
-		title: article.title,
-		article: article,
-		date: util.dateToYMD( article.date )
-	});
+    if ( article )
+        res.render( 'article', {
+            title: article.title,
+            article: article,
+            date: util.dateToYMD( article.date )
+        });
+    else
+        res.render( '404', {title: 'Not Found'} );
 });
 
 app.get( '/', (function(){
-	var counter = storage.get( 'visits' ) || 0;
+	var counter = app.get( 'storage' ).get( 'visits' ) || 0;
 
 	return function( req, res ){
 		res.render( 'index', {
+            title: 'asd',
 			articles: articles,
             visits: ++counter
 		});
